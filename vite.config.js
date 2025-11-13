11b94b6ab77ed6e6b2446209c9ee1b8e3f13dca0
Sasha_Gerzhik <girasik3000@gmail.com>
2025-11-13 19:40:33 +0200

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/space-cakes/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
